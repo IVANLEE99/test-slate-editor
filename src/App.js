5941e2324791cb4af47f70007623020d33690bcd
871246995 <871246995@qq.com>
2019-03-15 17:14:31 +0800

    slate example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// 导入 `State` 模型。
+import { Editor } from 'slate-react'
+import { Value } from 'slate'
+// 构建初始状态…
+const initialState = Value.fromJSON({
+  document: {
+    nodes: [
+      {
+        kind: 'block',
+        type: 'paragraph',
+        nodes: [
+          {
+            kind: 'text',
+            ranges: [
+              {
+                text: 'A line of text in a paragraph.'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+})
 
 class App extends Component {
+  // 设置应用创建时的初始状态。
+  state = {
+    value: initialState
+  }
+  // 发生变更时，使用新的编辑器状态更新应用的 React 状态。
+  onChange = ({ value }) => {
+    this.setState({ value })
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,6 +53,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Editor value={this.state.value} onChange={this.onChange} />
       </div>
     );
   }
