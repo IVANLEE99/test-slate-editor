3691254e9dd5862b5730e6f61c6d7ceeb58d8a73
871246995 <871246995@qq.com>
2019-03-18 09:44:54 +0800

    save to database

--- src/App.js
+++ src/App.js
@@ -1,62 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-// 导入 `State` 模型。
+// Switch to using the Plain serializer.
 import { Editor } from 'slate-react'
-import { Value } from 'slate'
-// 构建初始状态…
-const initialState = Value.fromJSON({
-  document: {
-    nodes: [
-      {
-        kind: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            kind: 'text',
-            ranges: [
-              {
-                text: 'A line of text in a paragraph.'
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-})
+import Plain from 'slate-plain-serializer'
 
-class App extends Component {
-  // 设置应用创建时的初始状态。
+const existingValue = localStorage.getItem('content')
+const initialValue = Plain.deserialize(
+  existingValue || 'A string of plain text.'
+)
+
+class App extends React.Component {
   state = {
-    value: initialState
+    value: initialValue,
   }
-  // 发生变更时，使用新的编辑器状态更新应用的 React 状态。
+
   onChange = ({ value }) => {
+    if (value.document != this.state.value.document) {
+      const content = Plain.serialize(value)
+      localStorage.setItem('content', content)
+    }
+
     this.setState({ value })
   }
 
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Editor value={this.state.value} onChange={this.onChange} />
-      </div>
-    );
+    return <Editor value={this.state.value} onChange={this.onChange} />
   }
 }
-
 export default App;
